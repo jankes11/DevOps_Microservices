2e3db6af4b72e6dc83755b087c998605619b351a
EC2 Default User <ec2-user@ip-172-31-60-81.ec2.internal>
2023-04-10 18:33:17 +0000

    first commitment

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="jankes11/predictions_app"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag predictions_app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
